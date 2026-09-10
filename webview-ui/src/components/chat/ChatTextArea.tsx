009e09f186818325607a1b40ea4cddebb6b31c73
Mikhail Sidorov <sidorov.mi@promail366.com>
2026-09-11 01:46:30 +0300

    Исправлена функциональность Shift+перетаскивание файлов в ChatTextArea
    Переместил e.preventDefault() перед проверкой клавиши Shift в обработчике onDragOver, чтобы восстановить возможность перетаскивания файлов с зажатой клавишей Shift. Ранее preventDefault вызывался только после проверки Shift, что блокировало стандартное поведение браузера.

--- webview-ui/src/components/chat/ChatTextArea.tsx
+++ webview-ui/src/components/chat/ChatTextArea.tsx
@@ -971,13 +971,14 @@ export const ChatTextArea = forwardRef<HTMLTextAreaElement, ChatTextAreaProps>(
 						className={cn("chat-text-area", !isEditMode && "relative", "flex", "flex-col", "outline-none")}
 						onDrop={handleDrop}
 						onDragOver={(e) => {
+							e.preventDefault()
+
 							// Only allowed to drop images/files on shift key pressed.
 							if (!e.shiftKey) {
 								setIsDraggingOver(false)
 								return
 							}
 
-							e.preventDefault()
 							setIsDraggingOver(true)
 							e.dataTransfer.dropEffect = "copy"
 						}}
